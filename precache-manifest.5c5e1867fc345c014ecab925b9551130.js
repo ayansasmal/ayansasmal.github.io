f5cd71bb80a4cb8aad75bab574721b04b526d330
Ayan Sasmal <ayandelhi@gmail.com>
2020-07-31 12:43:57 +1000

    update urls

---
+++ precache-manifest.5c5e1867fc345c014ecab925b9551130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44befc7e20dd32bf0e8674389d617bda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a401dada8a52151a0a70",
+    "url": "/static/css/main.b55b7e03.chunk.css"
+  },
+  {
+    "revision": "7c6b92dac350775fca5b",
+    "url": "/static/js/2.0c65f87d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c65f87d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a401dada8a52151a0a70",
+    "url": "/static/js/main.95a1c7a5.chunk.js"
+  },
+  {
+    "revision": "eb8c992a5d74082aa51b",
+    "url": "/static/js/runtime-main.eae234d2.js"
+  },
+  {
+    "revision": "e54004afb4a98cf88fb8555ec039c390",
+    "url": "/static/media/FB_IMG_1572578256965.e54004af.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1f728aa0f1d83a4d8505e0a72a33399a.js"
+  "/precache-manifest.5c5e1867fc345c014ecab925b9551130.js"
 );
 
 self.addEventListener('message', (event) => {
